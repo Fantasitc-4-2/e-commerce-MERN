b83cce923f8c3ca96de5745b4550f52afcd6cb15
Abdalkareem <kekkdead@gmail.com>
2025-09-24 18:38:46 +0300

    addind model layer

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,12 @@ const express = require('express')
 const app = express()
 const mongoose = require("mongoose")
 const port = 3000
-
+process.loadEnvFile()
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-
+mongoose.connect(process.env.DB_URI).then(()=>console.log("DB CONNECTED")).catch(err=>err.message);
 
 
 app.listen(port, () => {
