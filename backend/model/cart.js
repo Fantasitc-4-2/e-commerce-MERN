164727f02d1611ed28b0d303e1483f7511a849ed
Abdalkareem <kekkdead@gmail.com>
2025-09-24 18:50:09 +0300

    init backend structure

--- backend/model/cart.js
+++ backend/model/cart.js
@@ -2,7 +2,16 @@ const mongoose = require("mongoose");
 
 
 const CartSchema = new mongoose.Schema({
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        require:true
+    },
     items:{
-        
+        type:Array,
+        require:true,
+    },
+    totalPrice:{
+        type:Number
     }
 })
