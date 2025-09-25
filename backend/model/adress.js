6b175cdbb1c9c8084e9f7b1319340bf5fca36e89
mohammed Khaled <72397920+mohammed-khaled-5@users.noreply.github.com>
2025-09-25 19:58:53 +0300

    i added the user schema and adress schema (related to the user) and added the script in package.json (npm run dev) to use nodemon

---
+++ backend/model/adress.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const addressSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  street: String,
+  city: String,
+  zip: String,
+  country: String,
+}, { timestamps: true });
+
+const Address = mongoose.model("Address", addressSchema);
+module.exports = Address;
---
+++ backend/model/user.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const addressSchema = new mongoose.Schema({
+  street: String,
+  city: String,
+  zip: String,
+  country: String
+}, { _id: false });
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, trim: true },
+  //trim for save the input without spacing in the beginning or the end
+  //" Mohammed " will be "Mohammed"
+  email: { type: String, unique: true, required: true, lowercase: true, trim: true },
+  passwordHash: { type: String, required: true },
+  phoneNumber: { type: String, trim: true },
+
+  roles: { type: [String], enum: ["customer", "admin"], default: ["customer"] },
+
+  isVerified: { type: Boolean, default: false },
+  otp: String,
+  otpExpires: Date,
+  resetPasswordToken: String,
+  resetPasswordExpires: Date,
+
+  lastLogin: Date,
+
+  createdAt: { type: Date, default: Date.now }
+});
+
+
+// Optional: index for faster lookup
+userSchema.index({ email: 1 });
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
