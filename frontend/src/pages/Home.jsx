edbf68aba502e0dfd18d8dcce1b677005b27c4cc
Abdalkareem <kekkdead@gmail.com>
2025-09-27 19:32:04 +0300

    fixing tailwindCSS config

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className='bg-blue-500 text-white p-4 rounded-lg shadow-lg'>
+        <h1 className='text-2xl font-bold underline underline-offset-1'>
+            HELLO - Tailwind CSS is working!
+        </h1>
     </div>
   )
 }
