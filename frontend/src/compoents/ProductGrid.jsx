5042229cdffad49da3691e1e7134d72143d8ac9e
Nour <kkid607@gmail.com>
2025-09-29 22:08:23 +0300

    created a json file for products + fetching data in Product grid component

--- frontend/src/compoents/ProductGrid.jsx
+++ frontend/src/compoents/ProductGrid.jsx
@@ -1,16 +1,99 @@
-import React, { useState } from 'react'
-import ProductCard from './ProductCard'
+import React, { useEffect, useState } from "react";
+import ProductCard from "./ProductCard";
+import axios from "axios";
 
 export default function ProductGrid() {
-    const [data] = useState([
-        {title:"product 1",description:"product descritpion",img:"product-1.jpg"},{title:"product 2",description:"product descritpion",img:"product-2.jpg"},{title:"product 1",description:"product descritpion",img:"product-1.jpg"},{title:"product 2",description:"product descritpion",img:"product-2.jpg"},{title:"product 1",description:"product descritpion",img:"product-1.jpg"},{title:"product 2",description:"product descritpion",img:"product-2.jpg"},{title:"product 1",description:"product descritpion",img:"product-1.jpg"},{title:"product 2",description:"product descritpion",img:"product-2.jpg"},{title:"product 1",description:"product descritpion",img:"product-1.jpg"},{title:"product 2",description:"product descritpion",img:"product-2.jpg"},{title:"product 1",description:"product descritpion",img:"product-1.jpg"},{title:"product 2",description:"product descritpion",img:"product-2.jpg"},{title:"product 1",description:"product descritpion",img:"product-1.jpg"},{title:"product 2",description:"product descritpion",img:"product-2.jpg"}
-    ])
+  const [data] = useState([
+    {
+      title: "product 1",
+      description: "product descritpion",
+      img: "product-1.jpg",
+    },
+    {
+      title: "product 2",
+      description: "product descritpion",
+      img: "product-2.jpg",
+    },
+    {
+      title: "product 1",
+      description: "product descritpion",
+      img: "product-1.jpg",
+    },
+    {
+      title: "product 2",
+      description: "product descritpion",
+      img: "product-2.jpg",
+    },
+    {
+      title: "product 1",
+      description: "product descritpion",
+      img: "product-1.jpg",
+    },
+    {
+      title: "product 2",
+      description: "product descritpion",
+      img: "product-2.jpg",
+    },
+    {
+      title: "product 1",
+      description: "product descritpion",
+      img: "product-1.jpg",
+    },
+    {
+      title: "product 2",
+      description: "product descritpion",
+      img: "product-2.jpg",
+    },
+    {
+      title: "product 1",
+      description: "product descritpion",
+      img: "product-1.jpg",
+    },
+    {
+      title: "product 2",
+      description: "product descritpion",
+      img: "product-2.jpg",
+    },
+    {
+      title: "product 1",
+      description: "product descritpion",
+      img: "product-1.jpg",
+    },
+    {
+      title: "product 2",
+      description: "product descritpion",
+      img: "product-2.jpg",
+    },
+    {
+      title: "product 1",
+      description: "product descritpion",
+      img: "product-1.jpg",
+    },
+    {
+      title: "product 2",
+      description: "product descritpion",
+      img: "product-2.jpg",
+    },
+  ]);
+
+  const [products, setProducts] = useState([]);
+  useEffect(async () => {
+    try {
+      const res = await axios.get("http://localhost:5000/products");
+      setProducts(res.data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, []);
   return (
-    <div className='grid grid-cols-4 m-5 gap-4'>
-      {data.map(product=>(
-        <ProductCard title={product.title} img={product.img} description={product.description} />
-      )
-      )}
+    <div className="grid grid-cols-4 m-5 gap-4">
+      {data.map((product) => (
+        <ProductCard
+          title={product.title}
+          img={product.img}
+          description={product.description}
+        />
+      ))}
     </div>
-  )
+  );
 }
