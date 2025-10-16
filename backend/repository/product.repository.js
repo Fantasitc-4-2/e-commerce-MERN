da8cbc41b635d9537c533a362894c95316727791
mohammed Khaled <72397920+mohammed-khaled-5@users.noreply.github.com>
2025-10-16 15:12:40 +0300

    Added reviewCount and averageRating to GET all products

--- backend/repository/product.repository.js
+++ backend/repository/product.repository.js
@@ -2,23 +2,80 @@ import productModel from "../model/Product.js";
 import mongoose from "mongoose";
 export const getAllProducts = async (limit, page, searchVal, price, categoryId) => {
   const skip = (page - 1) * limit;
+
   const filter = {
-    $or: [
-      {title: { $regex: searchVal, $options: "i" }},
-      {description: { $regex: searchVal, $options: "i" },}
-    ]
+    $or: [{
+        title: {
+          $regex: searchVal,
+          $options: "i"
+        }
+      },
+      {
+        description: {
+          $regex: searchVal,
+          $options: "i"
+        }
+      },
+    ],
+  };
+
+  if (price !== undefined && !isNaN(price)) {
+    filter.price = {
+      $lte: Number(price)
+    };
   }
-  if(price !== undefined && !isNaN(price)) {
-    filter.price = { $lte: Number(price)};
-  }
+
   if (categoryId) {
     filter.category = new mongoose.Types.ObjectId(categoryId);
   }
-  console.log(filter);
-  return await productModel
-    .find(filter)
-    .skip(skip)
-    .limit(limit);
+
+  const products = await productModel.aggregate([{
+      $match: filter
+    },
+    {
+      $lookup: {
+        from: "reviews", // collection name (auto-created from model "Review")
+        localField: "_id",
+        foreignField: "product",
+        as: "reviews",
+      },
+    },
+    {
+      $addFields: {
+        reviewCount: {
+          $size: "$reviews"
+        },
+        averageRating: {
+          $divide: [{
+              $round: {
+                $multiply: [{
+                    $ifNull: [{
+                      $avg: "$reviews.ratings"
+                    }, 0]
+                  },
+                  2
+                ]
+              }
+            },
+            2
+          ]
+        },
+      },
+    },
+    {
+      $project: {
+        reviews: 0, // exclude review list from response
+      },
+    },
+    {
+      $skip: skip
+    },
+    {
+      $limit: limit
+    },
+  ]);
+
+  return products;
 };
 
 export const getProductById = async (id) => {
@@ -34,5 +91,8 @@ export const deleteProductById = async (id) => {
 }
 
 export const updateProductById = (id, updateData) => {
-  return Product.findByIdAndUpdate(id, updateData, { new: true, runValidators: true });
+  return Product.findByIdAndUpdate(id, updateData, {
+    new: true,
+    runValidators: true
+  });
 };
