271ad6438cb598ea0433000f5f22686224cf2fbf
fadyashraf8 <fadyashraf909@gmail.com>
2025-10-17 19:20:34 +0300

    edits in cart

--- backend/route/cart.route.js
+++ backend/route/cart.route.js
@@ -4,7 +4,10 @@ import auth from "../middleware/authMiddleware.js";
 
 const cartRouter = express.Router();
 
-cartRouter.route("/").post(auth, cartController.addToCart);
+cartRouter.route("/")
+.post(auth, cartController.addToCart)
+.get(auth, cartController.getLoggedUsercart)
+
 cartRouter.route("/:id").delete(auth, cartController.removeFromCart);
 cartRouter.route("/:id").put(auth, cartController.updateQuantity);
 
