ff3616bb11c440aca9673958286d74a861137298
neggmmm <kekkdead@gmail.com>
2025-11-02 15:22:58 +0200

    meta tag

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -6,11 +6,7 @@ const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 // Use environment variable or fallback to backend URL
 const api = axios.create({
-<<<<<<< HEAD
   baseURL: BASE_URL,
-=======
-  baseURL: import.meta.env.VITE_API_URL || "https://e-commerce-mern-five-sage.vercel.app/",
->>>>>>> cd389e935cfe80997fa3cc689836290e6b247fa3
   withCredentials: true,
   timeout: 10000,
 });
